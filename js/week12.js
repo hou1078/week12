02abeb9ede61b2bee4513e538eab583ccbffe91d
Your Name <you@example.com>
2023-03-27 15:14:15 -0700

    week12

---
+++ js/week12.js
@@ -0,0 +1,4 @@
+theButton.addEventListener('click', function(e){
+    e.preventDefault()// Prevent the default behavior of scrolling to the top
+    document.body.style.backgroundColor = "blue"; // Change the background color of the page to blue
+  });
