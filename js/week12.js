f2347dd3184bba2a33d29fff954ebf2342d14d45
Your Name <you@example.com>
2023-03-27 15:47:24 -0700

    button

--- js/week12.js
+++ js/week12.js
@@ -2,8 +2,27 @@ let buttonRef= document.querySelector("#clickme")
 
 function alertUser(){
     alert("You Click!!!")
-    // buttonRef.removeEventListener('click',alertUser)    
+    // buttonRef.removeEventListener('click',alertUser)  
 }
 
 buttonRef.addEventListener('click',alertUser,{once:true})
+
+function backgroundChange(){
+    document.body.classList.add("pink")
+}
+
+function changeText(){
+    if(buttonRef.textContent==='Click me!'){
+        buttonRef.textContent="Clicked";
+    }
+    else{
+        buttonRef.textContent="Click me!";
+    }
+}
+
+buttonRef.addEventListener('click',backgroundChange)
+buttonRef.addEventListener('click',changeText)  
+
+
+
 
