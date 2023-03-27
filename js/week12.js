f94d855bc9c7f5f418ac9a805c10aaee5c329928
Your Name <you@example.com>
2023-03-27 15:21:23 -0700

    alertUser

--- js/week12.js
+++ js/week12.js
@@ -1,4 +1,7 @@
-theButton.addEventListener('click', function(e){
-    e.preventDefault()// Prevent the default behavior of scrolling to the top
-    document.body.style.backgroundColor = "blue"; // Change the background color of the page to blue
-  });
+let buttonRef= document.querySelector("#clickme")
+
+function alertUser(){
+    alert("You Click!!!")
+}
+
+buttonRef.addEventListener('click',alertUser)
