c77f1c442f9440feb903c629396e07e05cf34a54
Your Name <you@example.com>
2023-03-27 15:26:22 -0700

    add and remove event

--- js/week12.js
+++ js/week12.js
@@ -2,6 +2,8 @@ let buttonRef= document.querySelector("#clickme")
 
 function alertUser(){
     alert("You Click!!!")
+    buttonRef.removeEventListener('click',alertUser)    
 }
 
 buttonRef.addEventListener('click',alertUser)
+
