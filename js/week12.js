75afd6909209b1845ea0c06927a6516455a8cd9b
Your Name <you@example.com>
2023-04-03 13:59:53 -0700

    change button text color

--- js/week12.js
+++ js/week12.js
@@ -35,13 +35,23 @@ const buttonContainer= document.querySelector(".button-container")
 function changeBGGreen(event){
     console.log(event.target.tagname)
     event.target.classList.add("green")
-    // if (event.target.tagName === 'BUTTON') {
-    //     const color = event.target.textContent;
-    //     event.target.style.backgroundColor = color;
-    //   }
+    if (event.target.tagName === 'BUTTON') {
+        const color = event.target.textContent;
+        event.target.style.backgroundColor = color;
+      }
     }
 buttonContainer.addEventListener('click',changeBGGreen)
-
-
-
+function changeBGGreen(event){
+    event.target.classList.add("greenBG")
+}
+buttonContainer.addEventListener("mouseover", changeBGGreen)
+function changeTextColor(event){
+    //event.target -> button
+    //if the text on the button says red -> the text color should be red
+    //event.target.textContent
+    console.log(event.target.textContent)
+    let myColor= event.target.textContent
+    event.target.classList.add(myColor)
+}
+buttonContainer.addEventListener("click",changeTextColor)
 
